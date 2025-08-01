2968f14a60ed6e5a633598a41b3d64c827de4037
VIVEK-THAKUR626 <vistaar4324@gmail.com>
2025-08-02 02:26:23 +0530

    complete the logic to make the game work as expected

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const playGame = document.getElementById("playGame");
 const goBack = document.getElementById("goBack");
 const proceed = document.getElementById("proceed");
+const home = document.getElementById("home");
 
-let humanScore = document.getElementById("humanScore");
-let computerScore = document.getElementById("computerScore");
+const humanScoreBoard = document.getElementById("humanScore");
+const computerScoreBoard = document.getElementById("computerScore");
 
-let humanPlayArea = document.getElementById("humanPlayArea");
-let computerPlayArea = document.getElementById("computerPlayArea");
+const humanPlayArea = document.querySelector(".humanPlayArea");
+const computerPlayArea = document.querySelector(".computerPlayArea");
 
 function computerChoices(min,max){
     const lowerBound = Math.ceil(min);
@@ -32,9 +33,83 @@ if (proceed) {
     })
 }
 
-humanPlayArea.addEventListener("keydown",(event)=>{
+if (home) {
+    home.addEventListener("click",() => {
+        window.location.href = "index.html";
+    })
+}
+var humanScore = 0;
+var computerScore = 0;
+
+document.addEventListener("keydown",(event)=>{
     event.preventDefault();
+
+    const computerChoice = computerChoices(1,4);
+
+    if(computerChoice === 1){
+        computerPlayArea.className = "stone";
+    }else if(computerChoice === 2){
+        computerPlayArea.className = "paper";
+    }else if(computerChoice === 3){
+        computerPlayArea.className = "scissors";
+    }
+
     if(event.key === "a"){
-        
+        humanPlayArea.className = "stone";
+        humanPlayArea.textContent = "STONE";
+
+        if(computerPlayArea.className === "stone"){
+            computerPlayArea.textContent = "STONE";
+
+        }else if(computerPlayArea.className === "paper"){
+            computerPlayArea.textContent = "PAPER";
+            computerScore += 1;
+            computerScoreBoard.textContent = `${computerScore}`;
+
+        }else if(computerPlayArea.className === "scissors"){
+            computerPlayArea.textContent = "SCISSORS";
+            humanScore += 1;
+            humanScoreBoard.textContent = `${humanScore}`;
+        }
+    }else if(event.key === "w"){
+        humanPlayArea.className = "paper";
+        humanPlayArea.textContent = "PAPER";
+        if(computerPlayArea.className === "stone"){
+            computerPlayArea.textContent = "STONE";
+            humanScore += 1;
+            humanScoreBoard.textContent = `${humanScore}`;
+        }else if(computerPlayArea.className === "paper"){
+            computerPlayArea.textContent = "PAPER";
+        }else if(computerPlayArea.className === "scissors"){
+            computerPlayArea.textContent = "SCISSORS";
+            computerScore += 1;
+            computerScoreBoard.textContent = `${computerScore}`;
+        }
+    }else if(event.key === "d"){
+        humanPlayArea.className = "scissors";
+        humanPlayArea.textContent = "SCISSORS";
+        if(computerPlayArea.className === "stone"){
+            computerPlayArea.textContent = "STONE";
+            computerScore += 1;
+            computerScoreBoard.textContent = `${computerScore}`;
+        }else if(computerPlayArea.className === "paper"){
+            computerPlayArea.textContent = "PAPER";
+            humanScore += 1;
+            humanScoreBoard.textContent = `${humanScore}`;
+        }else if(computerPlayArea.className === "scissors"){
+            computerPlayArea.textContent = "SCISSORS";
+        }
+    }else if(event.key === "q"){
+        alert("Real Players don't quit without a fight");
+    }
+
+    if (humanScore === 5) {
+        setTimeout(() => {
+            window.location.href = "won.html";
+        }, 1100);
+    } else if (computerScore === 5) {
+        setTimeout(() => {
+            window.location.href = "lost.html";
+        }, 1100);
     }
 })
